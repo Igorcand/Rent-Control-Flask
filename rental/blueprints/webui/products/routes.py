6bd19d6d488150827783a2d250bae33a0a57a827
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-08 07:38:13 -0300

    adding some routes

--- rental/blueprints/webui/products/routes.py
+++ rental/blueprints/webui/products/routes.py
@@ -1,7 +1,38 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for, flash
+from datetime import datetime
+from .forms import LoginForm
+from rental.models import User
 
 bp = Blueprint("products", __name__)
 
 @bp.route('/')
+def log():
+    return render_template('log.html') 
+
+@bp.route('/teste', methods=['GET', 'POST'])
+def teste():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        print(f'user = {user}')
+        if user:
+            flash(f"Welcome, {form.email.data}. You're logedin now", 'success')
+            return redirect(request.args.get('next') or url_for('webui.products.home'))
+        else:
+            flash(f'Wrong password plese try again', 'danger')
+        
+    return render_template('log2.html', form=form) 
+
+@bp.route('/register')
+def register():
+    return render_template('customer/register.html') 
+
+@bp.route('/home')
 def home():
-    return render_template('index.html')
+    data = datetime.today().strftime('%Y-%m-%d')
+    year, month, day = data.split('-')
+    print(f'year = {year}')
+    print(f'month = {month}')
+    print(f'day = {day}')
+
+    return render_template('index.html', day=day, month=month, year=year) 
