23bc497f8ed3a147ead16a8d7002d6f65fd05815
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-09 16:05:29 -0300

    added a bycript while tring user a password

--- rental/blueprints/webui/products/routes.py
+++ rental/blueprints/webui/products/routes.py
@@ -1,31 +1,38 @@
-from flask import render_template, Blueprint, request, redirect, url_for, flash
+from flask import render_template, Blueprint, request, redirect, url_for, flash, session
 from datetime import datetime
-from .forms import LoginForm
-from rental.models import User
+from .forms import LoginForm, RegistrationForm
+from rental.models import User as UserModel
+from rental.ext.crypt import bcrypt
+from rental.ext.database import db
 
 bp = Blueprint("products", __name__)
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def log():
-    return render_template('log.html') 
-
-@bp.route('/teste', methods=['GET', 'POST'])
-def teste():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
-        user = User.query.filter_by(email=form.email.data).first()
-        print(f'user = {user}')
-        if user:
+        user = UserModel.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data 
             flash(f"Welcome, {form.email.data}. You're logedin now", 'success')
             return redirect(request.args.get('next') or url_for('webui.products.home'))
         else:
             flash(f'Wrong password plese try again', 'danger')
-        
-    return render_template('log2.html', form=form) 
 
-@bp.route('/register')
+    return render_template('customer/log.html', form=form) 
+
+
+@bp.route('/register',  methods=['GET', 'POST'])
 def register():
-    return render_template('customer/register.html') 
+    form=RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = UserModel(name=form.name.data, username=form.username.data, email=form.email.data, password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Welcome, {form.name.data}. Thanks for registring', 'success')
+        return redirect(request.args.get('next') or url_for('webui.products.home'))
+    return render_template('customer/register.html', form=form) 
 
 @bp.route('/home')
 def home():
@@ -36,3 +43,4 @@ def home():
     print(f'day = {day}')
 
     return render_template('index.html', day=day, month=month, year=year) 
+
