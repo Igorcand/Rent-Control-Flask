4bc984f8881e8e3d1d60ce15e90b5552e7c40d98
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-09 06:36:51 -0300

    adde a form register

--- rental/blueprints/webui/products/forms.py
+++ rental/blueprints/webui/products/forms.py
@@ -5,3 +5,13 @@ from flask_wtf import FlaskForm, Form
 class LoginForm(Form):
     email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
     password = PasswordField('Password', [validators.DataRequired()])
+
+class RegistrationForm(Form):
+    name = StringField('Name', [validators.Length(min=4, max=25)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
