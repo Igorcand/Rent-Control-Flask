d8ae6873fcb4e60be338d1e674d1189f5c1cb19b
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-16 06:49:13 -0300

    configured admin

--- rental/ext/admin.py
+++ rental/ext/admin.py
@@ -1,7 +1,20 @@
 from flask_admin import Admin 
+from flask_admin.base import AdminIndexView
+from flask_admin.contrib import sqla
+from flask_simplelogin import SimpleLogin, login_required
 
+from .database import db
+from rental.models import User, Tenant, Address, Property
+
+AdminIndexView._handle_view = login_required(AdminIndexView._handle_view)
+sqla.ModelView._handle_view = login_required(sqla.ModelView._handle_view)
 admin = Admin()
 
 def init_app(app):
-    admin.name = app.config.get('ADMIN_NAME', 'Rental Controller')
+    admin.name = app.config.TITLE 
+    admin.template_mode = 'bootstrap3'
     admin.init_app(app)
+    admin.add_view(sqla.ModelView(User, db.session))
+    admin.add_view(sqla.ModelView(Tenant, db.session))
+    admin.add_view(sqla.ModelView(Address, db.session))
+    admin.add_view(sqla.ModelView(Property, db.session))
