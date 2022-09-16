409abb23ad85724c7677fd4f7703b203d6f3f5e7
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-16 06:48:57 -0300

    added last month line

--- rental/blueprints/restapi/resources/property.py
+++ rental/blueprints/restapi/resources/property.py
@@ -14,6 +14,7 @@ property_model = ns.model('Property',
     'value': fields.Integer(description='Property cpf '),
     'phone': fields.Integer(description='Property phone '),
     'pendancy': fields.Float(description='Property payment '),
+    'last_month': fields.Float(description='Property last_month payed '),
     'address_id': fields.Integer(description='Address id'),
     'tenant_id': fields.String(description='Tenat id'),
     'user_id': fields.Integer(description='user id'),
@@ -38,14 +39,15 @@ class Property(Resource):
         if property:
             ns.abort(404, description=f"This property id '{id}' already exist")
         name = data.get('name')
-        iptu = data.get('age')
-        value = data.get('cpf')
-        pendancy = data.get('phone')
-        address_id = data.get('payment')
-        tenant_id = data.get('entry')
+        iptu = data.get('iptu')
+        value = data.get('value')
+        pendancy = data.get('pendancy')
+        last_month = data.get('last_month')
+        address_id = data.get('address_id')
+        tenant_id = data.get('tenant_id')
         user_id = data.get('user_id')
 
-        property = PropertyModel(name=name, iptu=iptu, value=value, pendancy=pendancy, address_id=address_id, entenant_idtry=tenant_id, user_id=user_id)
+        property = PropertyModel(name=name, iptu=iptu, value=value, pendancy=pendancy, last_month=last_month, address_id=address_id, tenant_id=tenant_id, user_id=user_id)
         db.session.add(property)
         db.session.commit()
         return property
@@ -73,11 +75,14 @@ class PropertyResource(Resource):
 
             data=request.get_json()
             property.name=data.get('name')
-            property.age=data.get('iptu')
-            property.cpf=data.get('value')
-            property.phone=data.get('pendancy')
-            property.payment=data.get('address_id')
-            property.entry=data.get('tenant_id')
+            property.iptu=data.get('iptu')
+            property.value=data.get('value')
+            property.pendancy=data.get('pendancy')
+            property.last_month=data.get('last_month')
+            property.address_id=data.get('address_id')
+            property.tenant_id=data.get('tenant_id')
+            property.user_id=data.get('user_id')
+
             db.session.commit()
         else:
             data = request.get_json()
@@ -86,14 +91,15 @@ class PropertyResource(Resource):
             if property:
                 ns.abort(404, description=f"This property id '{id}' already exist")
             name = data.get('name')
-            iptu = data.get('age')
-            value = data.get('cpf')
-            pendancy = data.get('phone')
-            address_id = data.get('payment')
-            tenant_id = data.get('entry')
+            iptu = data.get('iptu')
+            value = data.get('value')
+            pendancy = data.get('pendancy')
+            last_month = data.get('last_month')
+            address_id = data.get('address_id')
+            tenant_id = data.get('tenant_id')
             user_id = data.get('user_id')
 
-            property = PropertyModel(name=name, iptu=iptu, value=value, pendancy=pendancy, address_id=address_id, entenant_idtry=tenant_id, user_id=user_id)
+            property = PropertyModel(name=name, iptu=iptu, value=value, pendancy=pendancy, last_month=last_month, address_id=address_id, tenant_id=tenant_id, user_id=user_id)
             db.session.add(property)
             db.session.commit()
 
