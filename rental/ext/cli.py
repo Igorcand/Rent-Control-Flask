07fc0a2d03a8a32522c861f12c179d213469e251
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-16 06:49:42 -0300

    added last month

--- rental/ext/cli.py
+++ rental/ext/cli.py
@@ -45,11 +45,11 @@ def init_app(app):
     def populate_tenant_db():
         """Populate db with sample data"""
         data = [
-            TenantModel(name="João", age=31,cpf=12345678910, phone=3190908080, payment=1200.00, entry=7, email='joao@email.com', activate=True, user_id=1),
-            TenantModel(name="Marcos", age=19,cpf=1234836910, phone=31977708080, payment=2000.00, entry=3, email='marcos@email.com', activate=True, user_id=1),
-            TenantModel(name="Marina", age=54,cpf=12322678910, phone=3192208080, payment=2200.00, entry=4, email='Marina@email.com', activate=True, user_id=2),
-            TenantModel(name="Pedro", age=19,cpf=55345678910, phone=5590908080, payment=1900.00, entry=1, email='pedro@email.com', activate=True, user_id=2),
-            TenantModel(name="Lidia", age=25,cpf=99345678910, phone=230908080, payment=1500.00, entry=2, email='lidia@email.com', activate=False, user_id=3),
+            TenantModel(name="João", age=31,cpf=12345678910, phone=3190908080, payment=1200.00, entry=7, expiration=10,email='joao@email.com', activate=True, user_id=1),
+            TenantModel(name="Marcos", age=19,cpf=1234836910, phone=31977708080, payment=2000.00, entry=3, expiration=12,email='marcos@email.com', activate=True, user_id=1),
+            TenantModel(name="Marina", age=54,cpf=12322678910, phone=3192208080, payment=2200.00, entry=4, expiration=1,email='Marina@email.com', activate=True, user_id=2),
+            TenantModel(name="Pedro", age=19,cpf=55345678910, phone=5590908080, payment=1900.00, entry=1, expiration=4, email='pedro@email.com', activate=True, user_id=2),
+            TenantModel(name="Lidia", age=25,cpf=99345678910, phone=230908080, payment=1500.00, entry=2, expiration=7, email='lidia@email.com', activate=False, user_id=3),
             
         ]
         db.session.bulk_save_objects(data)
@@ -60,11 +60,11 @@ def init_app(app):
     def populate_property_db():
         """Populate db with sample data"""
         data = [
-            PropertyModel(name="Galpão", iptu=310,value=1000000, pendency=False, address_id=1, tenant_id=1,user_id=1),
-            PropertyModel(name="Loja", iptu=550,value=500000, pendency=True, address_id=2, tenant_id=2,user_id=1),
-            PropertyModel(name="Casa", iptu=410,value=300000, pendency=False, address_id=3, tenant_id=3,user_id=2),
-            PropertyModel(name="Predio", iptu=980,value=800000, pendency=False, address_id=4, tenant_id=4,user_id=3),
-            PropertyModel(name="Kitnet", iptu=270,value=50000, pendency=False, address_id=1, tenant_id=5,user_id=3),
+            PropertyModel(name="Galpão", iptu=310,value=1000000, pendency=False, last_month=8,address_id=1, tenant_id=1,user_id=1),
+            PropertyModel(name="Loja", iptu=550,value=500000, pendency=True, last_month=8, address_id=2, tenant_id=2,user_id=1),
+            PropertyModel(name="Casa", iptu=410,value=300000, pendency=False, last_month=8, address_id=3, tenant_id=3,user_id=2),
+            PropertyModel(name="Predio", iptu=980,value=800000, pendency=False, last_month=8, address_id=4, tenant_id=4,user_id=3),
+            PropertyModel(name="Kitnet", iptu=270,value=50000, pendency=False, last_month=8, address_id=1, tenant_id=5,user_id=3),
 
             
         ]
@@ -94,22 +94,22 @@ def init_app(app):
         db.session.commit()
 
         data = [
-            TenantModel(name="João", age=31,cpf=12345678910, phone=3190908080, payment=1200.00, entry=7, email='joao@email.com', activate=True, user_id=1),
-            TenantModel(name="Marcos", age=19,cpf=1234836910, phone=31977708080, payment=2000.00, entry=3, email='marcos@email.com', activate=True, user_id=1),
-            TenantModel(name="Marina", age=54,cpf=12322678910, phone=3192208080, payment=2200.00, entry=4, email='Marina@email.com', activate=True, user_id=2),
-            TenantModel(name="Pedro", age=19,cpf=55345678910, phone=5590908080, payment=1900.00, entry=1, email='pedro@email.com', activate=True, user_id=2),
-            TenantModel(name="Lidia", age=25,cpf=99345678910, phone=230908080, payment=1500.00, entry=2, email='lidia@email.com', activate=False, user_id=3),
+            TenantModel(name="João", age=31,cpf=12345678910, phone=3190908080, payment=1200.00, entry=7, expiration=10,email='joao@email.com', activate=True, user_id=1),
+            TenantModel(name="Marcos", age=19,cpf=1234836910, phone=31977708080, payment=2000.00, entry=3, expiration=12,email='marcos@email.com', activate=True, user_id=1),
+            TenantModel(name="Marina", age=54,cpf=12322678910, phone=3192208080, payment=2200.00, entry=4, expiration=1,email='Marina@email.com', activate=True, user_id=2),
+            TenantModel(name="Pedro", age=19,cpf=55345678910, phone=5590908080, payment=1900.00, entry=1, expiration=4, email='pedro@email.com', activate=True, user_id=2),
+            TenantModel(name="Lidia", age=25,cpf=99345678910, phone=230908080, payment=1500.00, entry=2, expiration=7, email='lidia@email.com', activate=False, user_id=3),
             
         ]
         db.session.bulk_save_objects(data)
         db.session.commit()
 
         data = [
-            PropertyModel(name="Galpão", iptu=310,value=1000000, pendency=False, address_id=1, tenant_id=1,user_id=1),
-            PropertyModel(name="Loja", iptu=550,value=500000, pendency=True, address_id=2, tenant_id=2,user_id=1),
-            PropertyModel(name="Casa", iptu=410,value=300000, pendency=False, address_id=3, tenant_id=3,user_id=2),
-            PropertyModel(name="Predio", iptu=980,value=800000, pendency=False, address_id=4, tenant_id=4,user_id=3),
-            PropertyModel(name="Kitnet", iptu=270,value=50000, pendency=False, address_id=1, tenant_id=5,user_id=3),
+            PropertyModel(name="Galpão", iptu=310,value=1000000, pendency=False, last_month=8,address_id=1, tenant_id=1,user_id=1),
+            PropertyModel(name="Loja", iptu=550,value=500000, pendency=True, last_month=8,address_id=2, tenant_id=2,user_id=1),
+            PropertyModel(name="Casa", iptu=410,value=300000, pendency=False, last_month=8, address_id=3, tenant_id=3,user_id=2),
+            PropertyModel(name="Predio", iptu=980,value=800000, pendency=False, last_month=8,address_id=4, tenant_id=4,user_id=3),
+            PropertyModel(name="Kitnet", iptu=270,value=50000, pendency=False, last_month=8,address_id=1, tenant_id=5,user_id=3),
 
             
         ]
