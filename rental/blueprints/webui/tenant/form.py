789813d5ab47de3cb10018318c5fd85c9de7225f
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-09-20 07:44:59 -0300

    added some validators

--- rental/blueprints/webui/tenant/form.py
+++ rental/blueprints/webui/tenant/form.py
@@ -5,10 +5,10 @@ from flask_wtf.file import FileAllowed, FileField, FileRequired
 class TenantForm(FlaskForm):
     name = StringField('Name', [validators.DataRequired()])
     age = IntegerField('Age', [validators.DataRequired()])
-    cpf = IntegerField('CPF', [validators.DataRequired()])
-    phone = IntegerField('Phone', [validators.DataRequired()])
+    cpf = IntegerField('CPF', [validators.DataRequired(), validators.Length(min=11, max=11)])
+    phone = IntegerField('Phone', [validators.DataRequired(), validators.Length(min=9, max=14)])
     payment = DecimalField('Payment', [validators.DataRequired()])
     entry = IntegerField('Entry', [validators.DataRequired()])
     expiration = IntegerField('Expiration', [validators.DataRequired()])
-    email = StringField('Email', [validators.DataRequired()])
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
     activate = BooleanField('Activate', [validators.DataRequired()])
